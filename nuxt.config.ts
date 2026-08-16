09244cbe1fe7153b24af65c5af1bc0221bbcef04
percussionfloh <florian.moldaschl@web.de>
2026-08-16 15:31:55 +0200

    add leaflet (Map)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // nuxt.config.ts
 export default defineNuxtConfig({
   compatibilityDate: '2026-07-30',
-  modules: ['@nuxt/content', '@nuxt/ui'],
+  modules: ['@nuxt/content', '@nuxt/ui', '@nuxtjs/leaflet'],
 
   css: ['~/assets/main.css'],
 
