14c3cff09090279a98b8c1c0a9803a6f6cd2d74c
percussionfloh <florian.moldaschl@web.de>
2026-08-16 14:01:23 +0200

    make TheHeader

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,18 @@
+<script setup>
+import logo from '~/assets/logo_small_lila.png'
+
+
+useHead({
+  title: '40. DVSM-Nachwuchssymposium',
+  link: [
+    {
+      rel: 'icon',
+      href: logo
+    }
+  ]
+})
+</script>
+
 <template>
-  <div>
-Bruh
-  </div>
+  <TheHeader />
 </template>
---
+++ app/components/TheHeader.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+    <header>
+        <div class="grid grid-cols-[30%_40%_30%] justify-items-center p-4">
+            <div class="flex p-4">
+            <img src="/assets/mi.png" class="max-h-15 md:max-h-50" alt="Logo musica inaudita">
+            </div>
+            <div class="flex items-center">
+            <h1 class="text-3xl font-bold">40. DVSM-Nachwuchssymposium</h1>
+            </div>
+            <div class="flex p-4">
+            <img src="/assets/dvsm_logo.png" class="max-h-15 md:max-h-50" alt="Logo DVSM">
+            </div>
+        </div>
+        <div class="justify-items-center">
+            <h2 class="text-2xl font-bold">Critical Musicologies: Zwischen Kanonizität, Lehre und künstlerischer Praxis</h2>
+            <h3 class="text-xl font-bold">20.-22.11.2026, Berlin</h3>
+        </div>
+  </header>
+</template>
