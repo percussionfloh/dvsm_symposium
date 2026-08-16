644a0a99f37658bdfbd917e860f428eeaba660ba
percussionfloh <florian.moldaschl@web.de>
2026-08-16 15:44:10 +0200

    add zeitplan

---
+++ app/components/Zeitplan.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="flex flex-col items-center">
+        <a href="zeitplan.png" download>
+            <UButton class="bg-blue-500 hover:bg-blue-800" size="xl" :label="('Zeitplan herunterladen')" icon="i-lucide-download" />
+        </a>
+    </div>
+</template>
