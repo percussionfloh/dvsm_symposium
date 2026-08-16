be1989b3cc6f69c4cc63734a48373eb208da4798
percussionfloh <florian.moldaschl@web.de>
2026-08-16 11:44:29 +0200

    delete i18n lol

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,8 @@
 // nuxt.config.ts
 export default defineNuxtConfig({
   compatibilityDate: '2026-07-30',
-  modules: ['@nuxt/content', '@nuxt/ui', '@nuxtjs/i18n'],
+  modules: ['@nuxt/content', '@nuxt/ui'],
 
   css: ['~/assets/main.css'],
 
-  runtimeConfig: {
-
-  i18n: {
-    defaultLocale: 'de',
-    langDir: 'locales/',
-    locales: [
-      {
-        code: 'de',
-        file: 'de.yaml'
-      },
-      {
-        code: 'en',
-        file: 'en.yaml'
-      },
-    ]
-  } 
-}})
+})
