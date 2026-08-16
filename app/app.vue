087164ccb8ad84a035a36cdb9d268f62b407e31e
percussionfloh <florian.moldaschl@web.de>
2026-08-16 14:20:57 +0200

    add infotext

--- app/app.vue
+++ app/app.vue
@@ -15,4 +15,5 @@ useHead({
 
 <template>
   <TheHeader />
+  <Infotext />
 </template>
---
+++ app/components/Infotext.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="p-20 text-lg">
+        <p>Jährlich regt der Dachverband der Studierenden der Musikwissenschaften e.V. (DVSM) ein Nachwuchssymposium an, um dessen Konzeption und Durchführung sich Studierende bewerben können.
+        Für das 40. DVSM-Nachwuchssymposium wurde das Tagungskonzept von Musica inaudita mit dem Titel »Critical Musicologies: Zwischen Kanonizität, Lehre und künstlerischer Praxis« ausgewählt. 
+        In diesem Symposium sollen Kanones nicht nur in Konzertprogrammen, sondern auch in musikalischen Studiengängen, insbesondere der Musikwissenschaft, kritisch hinterfragt und damit Kanon- und Lehrkritik praktiziert werden.<br><br>
+
+        Der DVSM vernetzt Studierende der Musikwissenschaft im DACH-Raum und möchte dieses Netzwerk nutzen, um kanonkritische Diskurse zu teilen und weiterzuentwickeln. 
+        Auf dem DVSM-Nachwuchssymposium soll ein geschützter Raum entstehen, in dem gemeinsam produktiv Kritik geübt und Ansätze entwickelt werden, die in musikwissenschaftliche Institute und Musikhochschulen zurückwirken können.<br><br>
+        
+        Dabei ist es wichtig, zu betonen, dass es seit den 1980er Jahren viele Bemühungen und Publikationen gibt, die diese Ansätze bereits verfolgen.¹
+        Trotzdem werden weiterhin exkludierende Strukturen – bewusst oder unbewusst – reproduziert, was die Kontinuitäten dieser aufzeigt. 
+        Das Symposium knüpft an eine kanonkritische Praxis an und versteht sich zugleich als Beitrag zu musikwissenschaftlichen Debatten um Kanones, 
+        Macht und Historiographie, wie sie u. a. im Kontext von Kanonforschung, Gender Studies, postkolonialen und dekolonialen Theorien sowie Analysen institutioneller Normativität geführt werden.
+        Das Symposium als klassische Struktur von Redebeiträgen einzelner Personen soll durch die Möglichkeit alternativer Vortrags- und Austauschformate partizipativ und interdisziplinär gestaltet werden. 
+        Beiträge aus den Sparten der künstlerischen Forschung und Praxis, wie Performances, installative Arbeiten sowie Workshops werden als Formate der (Wissens-)Vermittlung anerkannt und gleichrangig zu rein inhaltlichen Vorträgen gesehen.
+        Zu Beginn der DVSM-Symposienreihe Ende der 1980er-Jahre war die Einbindung künstlerischer Programmpunkte gängige Praxis – dies soll wieder aufgenommen und weitergedacht werden. 
+        Die Tagung leistet somit einen Beitrag zum Verständnis von Wissensweitergabe, bei der das Hören und Denken als körperliche Praxis im Mittelpunkt stehen.</p>
+    </div>
+</template>
