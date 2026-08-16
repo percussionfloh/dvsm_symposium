806227f3b10fc4aa291f5a991b154a1c5694c1ea
percussionfloh <florian.moldaschl@web.de>
2026-08-16 18:58:21 +0200

    add awareness

---
+++ app/components/Awareness.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="text-center">
+        <div class="font-bold text-2xl text-violet-800">
+            <h2>Awareness-Konzept</h2>
+        </div>
+        
+        <div class="text-lg">
+            Awareness-Konzept folgt...
+        </div>
+        
+    </div>
+</template>
