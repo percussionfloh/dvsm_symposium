606f2c54f2c8a989669c26b10c3739a8b6f1ae8a
percussionfloh <florian.moldaschl@web.de>
2026-09-11 20:28:53 +0200

    set color to light

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,10 @@
 <script setup>
 import logo from '~/assets/logo_small_lila.png'
 
+const colorMode = useColorMode()
+
+colorMode.preference = 'light'
+colorMode.value = 'light'
 
 useHead({
   title: '40. DVSM-Nachwuchssymposium',
@@ -14,14 +18,15 @@ useHead({
 </script>
 
 <template>
-      <TheHeader />
-    <div class="grid grid-rows-[1fr_0.5fr_0.2fr_0.8fr_0.7fr_1fr] gap-10 pt-20 px-30">
-      <Infotext />
-      <Anmeldung />
-      <Zeitplan />
-      <Anfahrt />
-      <Awareness />
-      <Zugang />
-      <Mi/>
-  </div>
+    <TheHeader />
+      <div class="grid grid-rows-[1fr_0.5fr_0.2fr_0.8fr_0.7fr_1fr] gap-10 pt-20 px-30">
+        <Infotext />
+        <Anmeldung />
+        <Zeitplan />
+        <Anfahrt />
+        <Awareness />
+        <Zugang />
+        <Mi/>
+      </div>
+    <TheFooter />
 </template>
