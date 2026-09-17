a704f31a33a8a8bf9d92c8f9a0ce88883c722851
percussionfloh <florian.moldaschl@web.de>
2026-09-17 10:57:56 +0200

    fix header

--- app/components/TheHeader.vue
+++ app/components/TheHeader.vue
@@ -4,20 +4,22 @@
 
 <template>
     <header>
-        <div class="grid grid-cols-[30%_40%_30%] justify-items-center p-4">
-            <div class="flex p-4">
-            <img src="/assets/mi.png" class="max-h-15 md:max-h-50" alt="Logo musica inaudita">
+        <div class="w-full">  
+            <div class="grid grid-cols-[25%_50%_25%] justify-items-center p-4">
+                <div class="flex p-1 sm:p-2 md:p-4">
+                <img src="/assets/mi.png" class="max-h-15 sm:max-h-30 md:max-h-40 lg:max-h-50" alt="Logo musica inaudita">
+                </div>
+                <div class="flex items-center">
+                <h1 class="text-lg sm:text-2xl md:text-3xl lg:text-4xl font-bold">40. DVSM-Nachwuchssymposium</h1>
+                </div>
+                <div class="flex p-4">
+                <img src="/assets/dvsm_logo.png" class="max-h-15 sm:max-h-30 md:max-h-40 lg:max-h-50" alt="Logo DVSM">
+                </div>
             </div>
-            <div class="flex items-center">
-            <h1 class="text-4xl font-bold">40. DVSM-Nachwuchssymposium</h1>
-            </div>
-            <div class="flex p-4">
-            <img src="/assets/dvsm_logo.png" class="max-h-15 md:max-h-50" alt="Logo DVSM">
+            <div class="justify-items-center text-center p-4">
+                <h2 class="text-lg sm:text-2xl md:text-2.5xl lg:text-3xl font-bold">Critical Musicologies: Zwischen Kanonizität, Lehre und künstlerischer Praxis</h2>
+                <h3 class="text-lg sm:text-2xl md:text-2.5xl lg:text-3xl font-bold">20.-22.11.2026, Berlin</h3>
             </div>
         </div>
-        <div class="justify-items-center">
-            <h2 class="text-3xl font-bold">Critical Musicologies: Zwischen Kanonizität, Lehre und künstlerischer Praxis</h2>
-            <h3 class="text-2xl font-bold">20.-22.11.2026, Berlin</h3>
-        </div>
-  </header>
+    </header>
 </template>
