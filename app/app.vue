da44fa8c5d5798aff0fd5b9f1a15a643bbfdda04
percussionfloh <florian.moldaschl@web.de>
2026-09-17 10:57:37 +0200

    fix styling of App.vue

--- app/app.vue
+++ app/app.vue
@@ -18,15 +18,23 @@ useHead({
 </script>
 
 <template>
+  <div class="">
     <TheHeader />
-      <div class="grid grid-rows-[1fr_0.5fr_0.2fr_0.8fr_0.7fr_1fr] gap-10 pt-20 px-30">
-        <Infotext />
-        <Anmeldung />
-        <Zeitplan />
-        <Anfahrt />
-        <Awareness />
-        <Zugang />
-        <Mi/>
-      </div>
-    <TheFooter />
+  </div>
+  <div class="w-full grid 
+              grid-rows-[1fr_0.5fr_0.1fr_0.5fr_0.7fr_1fr]
+              md:grid-rows-[1fr_0.5fr_0.2fr_0.5fr_0.7fr_1fr]
+              gap-3 pt-4 px-4 
+              sm:gap-6 sm:pt-9 sm:px-15 
+              md:gap-10 md:pt-20 md:px-20 
+              lg:gap-10 lg:px-20 lg:px-30">
+    <Infotext />
+    <Anmeldung />
+    <Zeitplan />
+    <Anfahrt />
+    <Awareness />
+    <Zugang />
+    <Mi/>
+  </div>
+  <TheFooter />
 </template>
