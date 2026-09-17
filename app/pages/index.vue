d691156b95944a686451d76c60bf7b0460b6699d
percussionfloh <florian.moldaschl@web.de>
2026-09-17 18:35:50 +0200

    add welcome message

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -19,12 +19,15 @@ useHead({
 
 <template>
   <div class="w-full grid 
-              grid-rows-[1fr_0.5fr_0.1fr_0.5fr_0.7fr_1fr]
-              md:grid-rows-[1fr_0.5fr_0.2fr_0.5fr_0.7fr_1fr]
+              grid-rows-[0.5fr_1fr_0.5fr_0.1fr_0.5fr_0.7fr_1fr]
+              md:grid-rows-[0.5_1fr_0.5fr_0.2fr_0.5fr_0.7fr_1fr]
               gap-3 pt-4 px-4 
               sm:gap-6 sm:pt-9 sm:px-15 
               md:gap-10 md:pt-20 md:px-20 
               lg:gap-10 lg:px-20 lg:px-30">
+    <p class="text-xl">
+      Willkommen bei unserem Symposium BLABLABLA
+    </p>
     <Infotextx />
     <Anmeldungx />
     <Zeitplanx />
