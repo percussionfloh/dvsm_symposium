4ee943dddd330a547baaec3adf3d7313a9a0106a
percussionfloh <florian.moldaschl@web.de>
2026-09-17 18:27:29 +0200

    add styling to all pages

--- app/pages/zugang.vue
+++ app/pages/zugang.vue
@@ -2,5 +2,10 @@
 </script>
 
 <template>
-    <Zugangx />
+    <div class="gap-3 pt-4 px-4 
+                sm:gap-6 sm:pt-9 sm:px-15 
+                md:gap-10 md:pt-20 md:px-20 
+                lg:gap-10 lg:px-20 lg:px-30">
+        <Zeitplanx />
+    </div>
 </template>
