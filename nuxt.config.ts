3df635c64048b96ef7e88af13490d2f74f2d8694
percussionfloh <florian.moldaschl@web.de>
2026-09-17 11:18:40 +0200

    install i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // nuxt.config.ts
 export default defineNuxtConfig({
   compatibilityDate: '2026-07-30',
-  modules: ['@nuxt/ui', '@nuxtjs/leaflet'],
+  modules: ['@nuxt/ui', '@nuxtjs/leaflet', '@nuxtjs/i18n'],
 
   css: ['~/assets/main.css'],
 
