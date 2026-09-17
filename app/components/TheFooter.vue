93ad24b9711ae38139e8814479b5dabf64f0f429
percussionfloh <florian.moldaschl@web.de>
2026-09-17 18:27:35 +0200

    add copyright

--- app/components/TheFooter.vue
+++ app/components/TheFooter.vue
@@ -1,5 +1,5 @@
 <script setup>
-
+const currentYear = new Date().getFullYear()
 </script>
 
 <template>
@@ -13,5 +13,8 @@
             <div class="flex p-4">
             <img src="/assets/udk_logo.png" class="max-h-12 md:max-h-35" alt="Logo UDK">
         </div>
+    </div>
+    <div class="border-t border-gray-200 py-6">
+        <p class="text-center text-sm text-gray-500">© {{ currentYear }}, Florian Moldaschl. Alle Rechte vorbehalten.</p>
     </div>
 </template>
