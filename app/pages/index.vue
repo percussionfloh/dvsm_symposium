c2c2d4ffc9abea22302cb512db1d507e4db14ffd
percussionfloh <florian.moldaschl@web.de>
2026-09-18 15:17:15 +0200

    fix index

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -19,8 +19,8 @@ useHead({
 
 <template>
   <div class="w-full grid 
-              grid-rows-[0.5fr_1fr_0.5fr_0.1fr_0.5fr_0.7fr_1fr]
-              md:grid-rows-[0.5_1fr_0.5fr_0.2fr_0.5fr_0.7fr_1fr]
+              grid-rows-[1fr_0.5fr_0.1fr_0.5fr_0.7fr_1fr]
+              md:grid-rows-[1fr_0.5fr_0.2fr_0.5fr_0.7fr_1fr]
               gap-3 pt-4 px-4 
               sm:gap-6 sm:pt-9 sm:px-15 
               md:gap-10 md:pt-20 md:px-20 
