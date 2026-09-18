4c124698db46aa5026a5bb37a261d660d9adf58e
percussionfloh <florian.moldaschl@web.de>
2026-09-18 15:12:33 +0200

    fix index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -25,9 +25,6 @@ useHead({
               sm:gap-6 sm:pt-9 sm:px-15 
               md:gap-10 md:pt-20 md:px-20 
               lg:gap-10 lg:px-20 lg:px-30">
-    <p class="text-xl">
-      Willkommen bei unserem Symposium BLABLABLA
-    </p>
     <Infotextx />
     <Anmeldungx />
     <Zeitplanx />
