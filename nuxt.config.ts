72e1de8c47bc15eb2f86b82b139215516f7c08ce
percussionfloh <florian.moldaschl@web.de>
2026-09-23 12:18:47 +0200

    install @nuxt/scripts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,29 @@
 // nuxt.config.ts
 export default defineNuxtConfig({
   compatibilityDate: '2026-07-30',
-  modules: ['@nuxt/ui', '@nuxtjs/leaflet', '@nuxtjs/i18n'],
+  modules: ['@nuxt/ui', '@nuxtjs/leaflet', '@nuxtjs/i18n', '@nuxt/scripts'],
 
   css: ['~/assets/main.css'],
 
+  i18n: {
+    defaultLocale: 'de',
+    langDir: 'locales/',
+
+    locales: [
+      {
+        code: 'de',
+        name: 'Deutsch',
+        file: 'de.yaml'
+      },
+      {
+        code: 'de-easy',
+        name: 'Einfache Sprache',
+        file: 'de-easy.yaml'
+      }]
+    },
   
     app: {
-    baseURL: '/',
-    
-    head: {
-    htmlAttrs: {
-        lang: 'de'
-      }
-    }
+    baseURL: '/'
   }
-
 })
 
