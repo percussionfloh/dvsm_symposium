c0e0311d4140d16c719f631a2ebaee49710d751c
percussionfloh <florian.moldaschl@web.de>
2026-09-23 12:40:59 +0200

    delete scripts and easy de

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,15 @@
 // nuxt.config.ts
 export default defineNuxtConfig({
   compatibilityDate: '2026-07-30',
-  modules: ['@nuxt/ui', '@nuxtjs/leaflet', '@nuxtjs/i18n', '@nuxt/scripts'],
+  modules: ['@nuxt/ui', '@nuxtjs/leaflet', '@nuxtjs/i18n'],
 
   css: ['~/assets/main.css'],
 
   i18n: {
     defaultLocale: 'de',
     langDir: 'locales/',
-
-    locales: [
-      {
-        code: 'de',
-        name: 'Deutsch',
-        file: 'de.yaml'
-      },
-      {
-        code: 'de-easy',
-        name: 'Einfache Sprache',
-        file: 'de-easy.yaml'
-      }]
     },
-  
+    
     app: {
     baseURL: '/'
   }
