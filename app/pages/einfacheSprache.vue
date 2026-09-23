33de3a7b08a4e09e182cb4aa3fc949ef6966091f
percussionfloh <florian.moldaschl@web.de>
2026-09-23 13:29:17 +0200

    update einfacheSprache: verlassen

--- app/pages/einfacheSprache.vue
+++ app/pages/einfacheSprache.vue
@@ -2,5 +2,18 @@
 </script>
 
 <template>
-    hal
+
+    <div class="gap-3 pt-4 px-4 
+                sm:gap-6 sm:pt-9 sm:px-15 
+                md:gap-10 md:pt-20 md:px-20 
+                lg:gap-10 lg:px-20 lg:px-30">
+    Einfache Sprache-Version kommt bald!
+    </div>
+    <div class="flex justify-center items-center p-4">
+        <UButton to="/" color="white" size="xxl"> Einfache Sprache verlassen
+        <template #trailing>
+            <UIcon name="my-icon:icon-leichte-sprache-klein" class="size-5 md:size-8 ml-2"/>
+        </template>
+        </UButton>
+    </div>
 </template>
