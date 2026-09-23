bb7839efca13bf4c4cf21255683ed544db5fff14
percussionfloh <florian.moldaschl@web.de>
2026-09-23 12:47:46 +0200

    delete mi einfache sprache

--- app/components/Mix.vue
+++ app/components/Mix.vue
@@ -1,21 +1,5 @@
 <script setup>
-const { locale, locales, setLocale } = useI18n()
 
-
-function useDoc(name) {
-  return useAsyncData(
-    name,
-    () =>
-      queryCollection('docs')
-        .path(`/${locale.value}/${name}`)
-        .first(),
-    {
-      watch: [locale]
-    }
-  )
-}
-
-const { data: mi } = await useDoc('mi')
 </script>
 
 <template>
@@ -25,10 +9,6 @@ const { data: mi } = await useDoc('mi')
             <h2 class="text-2xl text-pink-600 font-bold">Musica inaudita</h2>
         </div>
         <div class="text-lg p-5">
-            <div class="text-justify pb-5">
-  <article>
-          <ContentRenderer v-if="waswird" :value="mi" />
-        </article> 
                         <p  class="pb-5">
                 Musica inaudita ist eine seit 2021 bestehende Initiative der Universität der Künste Berlin, die mit ihrer Arbeit auf strukturell diskriminierende Realitäten der ‚klassischen’ Musikwelt aufmerksam macht.
                 Komponistinnen und Musikerinnen, die nicht <em>weiß²</em>, heteronormativ, abled und cis-männlich sind, werden im Musikbetrieb marginalisiert.
@@ -37,22 +17,19 @@ const { data: mi } = await useDoc('mi')
                     Kanonizität kritisch einzuordnen und Alternativen zu präsentieren. Durch die Konzerte wird der Musik von marginalisierten Komponistinnen eine Bühne geboten.
                 Neben Konzerten bietet die Initiative musikwissenschaftliche Workshops an und pflegt eine Datenbank, die Zugang zu Noten von und Informationen zu marginalisierten Komponist*innen bietet. 
                 </p>
-
-
-                <p class="text-sm">
-                    ² Der Begriff „weiß“ wird klein und kursiv geschrieben, da er nicht die neutrale Beschreibung der Hautfarbe, sondern die Privilegierung und gesellschaftliche Machtposition von Menschen ohne Rassismuserfahrungen meint.
-                </p>
-            </div>
-            <div class="p-5">
-                <p>
-                Organisationsteam:<br>
-                Filip Bayer-Čech (FU Berlin / HU Berlin, er/ihm)<br>
-                Florian Moldaschl (HfM Freiburg, er/ihm)<br>
-                Emilia Reiter (FU Berlin / HU Berlin, keine/dey/demm)<br>
-                Marie-A. Schwebe (UdK Berlin, sie/ihr)<br>
-                Jelena Wißmann (HfM Detmold, sie/ihr)<br>
-                </p>
-            </div>
+            <p class="text-sm">
+                ² Der Begriff „weiß“ wird klein und kursiv geschrieben, da er nicht die neutrale Beschreibung der Hautfarbe, sondern die Privilegierung und gesellschaftliche Machtposition von Menschen ohne Rassismuserfahrungen meint.
+            </p>
+        </div>
+        <div class="p-5">
+            <p>
+            Organisationsteam:<br>
+            Filip Bayer-Čech (FU Berlin / HU Berlin, er/ihm)<br>
+            Florian Moldaschl (HfM Freiburg, er/ihm)<br>
+            Emilia Reiter (FU Berlin / HU Berlin, keine/dey/demm)<br>
+            Marie-A. Schwebe (UdK Berlin, sie/ihr)<br>
+            Jelena Wißmann (HfM Detmold, sie/ihr)<br>
+            </p>
         </div>
     </div>
 </template>
