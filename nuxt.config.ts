116f84192544ef1422008b298abcba3407cfdcdf
percussionfloh <florian.moldaschl@web.de>
2026-09-23 13:14:34 +0200

    add icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
-// nuxt.config.ts
+import { createResolver } from "nuxt/kit"
+
+const { resolve } = createResolver(import.meta.url)
+
 export default defineNuxtConfig({
   compatibilityDate: '2026-07-30',
-  modules: ['@nuxt/ui', '@nuxtjs/leaflet', '@nuxtjs/i18n'],
+  modules: ['@nuxt/ui', '@nuxtjs/leaflet', '@nuxtjs/i18n', '@nuxt/icon'],
 
   css: ['~/assets/main.css'],
 
@@ -12,6 +15,14 @@ export default defineNuxtConfig({
     
     app: {
     baseURL: '/'
+    },
+    icon: {
+    customCollections: [
+      {
+        prefix: 'my-icon',
+        dir: resolve('./app/assets/my-icons'),
+      },
+    ]
   }
 })
 
