cbbf1d48884f82abac65f23698c79034bfdf3cae
percussionfloh <florian.moldaschl@web.de>
2026-09-25 12:34:57 +0200

    add | to zeitplan

--- app/components/Zeitplanx.vue
+++ app/components/Zeitplanx.vue
@@ -12,15 +12,15 @@
             Hörsaal</h3>
         <p class="pt-10 leading-[1.8]">
             13:00 Uhr: Kaffee und Kennenlernen <br> <br>
-            14:00 Uhr: Begrüßung Musica inaudita <br> <br>
-            14:30 Uhr: Lecture Performance, Maebh Murphy (sie/ihr): Vergleichende Musikwissenschaft? <br> <br>
-            15:00 Uhr: Vortrag, Lily Hußmann (they/them/dey/deren): Roll Over Beethoven, Tell Carl Dahlhaus the News? Marxistische Popmusikforschung als Problem und Alternative zur (west)deutschen Musikwissenschaft der 1970er Jahre.<br> <br>
+            14:00 Uhr: Begrüßung von Musica inaudita und dem Dachverband der Studierenden der Musikwissenschaften e.V. (DVSM) <br> <br>
+            14:30 Uhr: Lecture Performance | Maebh Murphy (sie/ihr): Vergleichende Musikwissenschaft? <br> <br>
+            15:00 Uhr: Vortrag | Lily Hußmann (they/them/dey/deren): Roll Over Beethoven, Tell Carl Dahlhaus the News? Marxistische Popmusikforschung als Problem und Alternative zur (west)deutschen Musikwissenschaft der 1970er Jahre.<br> <br>
             15:30 Uhr: Bewegte Pause <br> <br>
-            16:00 Uhr: Vortrag, Leander von Criegern (alle), Laris Bäucker (er/ihm): Vom Klang zur Performance. Relationale Praktiken in der Neuen Musik <br> <br>
-            16:30 Uhr: Performance, Peter Tracy (er/ihm): Musik der lebenden deutschen Komponistin Eva-Maria Houben im Kontext partizipativer Open Scores diskutiert und vermittelt <br> <br>
-            17:30 Uhr: Vortrag, Nelli Tatarenko: Kurzvideos als alternative Form musikwissenschaftlicher Wissensvermittlung auf Grundlage des Social Media Musikkanals Know Your Tune <br> <br>
+            16:00 Uhr: Vortrag | Leander von Criegern (alle), Laris Bäucker (er/ihm): Vom Klang zur Performance. Relationale Praktiken in der Neuen Musik <br> <br>
+            16:30 Uhr: Performance | Peter Tracy (er/ihm): Musik der lebenden deutschen Komponistin Eva-Maria Houben im Kontext partizipativer Open Scores diskutiert und vermittelt <br> <br>
+            17:30 Uhr: Vortrag | Nelli Tatarenko: Kurzvideos als alternative Form musikwissenschaftlicher Wissensvermittlung auf Grundlage des Social Media Musikkanals Know Your Tune <br> <br>
             18:00 Uhr: Abendpause <br> <br>
-            19:30 Uhr: Abendprogramm, Helena Ernst (sie/ihr), Karin Zöllner (sie/ihr), Ronja Andersson (sie/ihr), Yuma Kruse (er/ihm): Werke von Sohui Jeong, Amy Beach...
+            19:30 Uhr: Abendprogramm <br> Helena Ernst (sie/ihr) <br> Karin Zöllner (sie/ihr) <br> Ronja Andersson (sie/ihr), Yuma Kruse (er/ihm) <br> mit Werken u.a. von Sohui Jeong und Amy Beach.
         </p>
     </div>
     <div class="px-10 md:px-30 pt-20">
@@ -32,21 +32,21 @@
             Kammersaal</h3>
         <p class="pt-10 leading-[1.8]">
             09:30 Uhr: Frühstück & Light-Morning-Rave<br> <br>
-            10:00 Uhr: Vortrag, Neema Kurth (sie/ ihr): Is Techno Black Again? Diskursive Strategien der Bedeutungsproduktion und -verschiebung im zeitgenössischen Techno am Beispiel des TIBA Festivals<br> <br>
-            10:30 Uhr: Vortrag, Isabelle Kretschmer (sie/ihr): Silenc-es/-ing': Intersektionale Perspektiven in Musicals am Beispiel von 'A Strange Loop', 'Fun Home', 'Hedwig and the Angry Inch' und 'The Color Purple' <br> <br>
-            11:00 Uhr: Vortrag, Tamina Pamir (sie/ihr): “Do you have a K-Pop Bias?” Zur Aushandlung von Präferenzen für K-Pop-Gruppen: Eine qualitative Interviewstudie <br> <br>
-            11:30 Uhr: Bewegte Pause, Rosanna Lovell & Studierende der Universität Hildesheim <br> <br>
-            12:00 Uhr: Workshop, Stefanie Bräuml (sie/ihr): Standardstücke! Standardwege?: Repertoirevorgaben als Kanonpraxis in der Ausbildung von Instrumental- und Gesangspädagog:innen und dem Unterricht an Musikschulen <br> <br>
-            13:00 Uhr: Vortrag, Rosa Elvira Bandera Sotelo (sie/ihr): Künstlerische Forschung als Mittel für neue Formen des Wissenserwerbs <br> <br>
-            13:30 Uhr: Vortrag, Seonhwa Lee: Zwischen Kanon und künstlerischer Praxis: Emilie Mayer und ihre Cellosonaten <br> <br>
+            10:00 Uhr: Vortrag | Neema Kurth (sie/ ihr): Is Techno Black Again? Diskursive Strategien der Bedeutungsproduktion und -verschiebung im zeitgenössischen Techno am Beispiel des TIBA Festivals<br> <br>
+            10:30 Uhr: Vortrag | Isabelle Kretschmer (sie/ihr): Silenc-es/-ing': Intersektionale Perspektiven in Musicals am Beispiel von 'A Strange Loop', 'Fun Home', 'Hedwig and the Angry Inch' und 'The Color Purple' <br> <br>
+            11:00 Uhr: Vortrag | Tamina Pamir (sie/ihr): “Do you have a K-Pop Bias?” Zur Aushandlung von Präferenzen für K-Pop-Gruppen: Eine qualitative Interviewstudie <br> <br>
+            11:30 Uhr: Bewegte Pause | Rosanna Lovell & Studierende der Universität Hildesheim <br> <br>
+            12:00 Uhr: Workshop | Stefanie Bräuml (sie/ihr): Standardstücke! Standardwege?: Repertoirevorgaben als Kanonpraxis in der Ausbildung von Instrumental- und Gesangspädagog:innen und dem Unterricht an Musikschulen <br> <br>
+            13:00 Uhr: Vortrag | Rosa Elvira Bandera Sotelo (sie/ihr): Künstlerische Forschung als Mittel für neue Formen des Wissenserwerbs <br> <br>
+            13:30 Uhr: Vortrag | Seonhwa Lee: Zwischen Kanon und künstlerischer Praxis: Emilie Mayer und ihre Cellosonaten <br> <br>
             14:00 Uhr: Mittagspause <br> <br>
-            15:00 Uhr: Performance, widerStimme: unbeSTIMMte Träume – who cares?<br> <br>
-            16:00 Uhr: Vortrag, Fee-Noa Wendsche (keine): Backstage Awareness - Antidiskriminierung für marginalisierte Musiker*innen <br> <br>
-            16:30 Uhr: Vortrag, Marie Schürmann: Wirtschaftliche Macht und Kulturelle Gleichberechtigung. Das Zusammenspiel von Finanzierungsmechanismen und der Repräsentation marginalisierter Komponist*innen in der Programmgestaltung professioneller Orchester<br> <br>
+            15:00 Uhr: Performance | widerStimme: unbeSTIMMte Träume – who cares?<br> <br>
+            16:00 Uhr: Vortrag | Fee-Noa Wendsche (keine): Backstage Awareness - Antidiskriminierung für marginalisierte Musiker*innen <br> <br>
+            16:30 Uhr: Vortrag | Marie Schürmann: Wirtschaftliche Macht und Kulturelle Gleichberechtigung. Das Zusammenspiel von Finanzierungsmechanismen und der Repräsentation marginalisierter Komponist*innen in der Programmgestaltung professioneller Orchester<br> <br>
             17:00 Uhr: Kaffeepause <br> <br>
-            17:15 Uhr: Workshop, Leander von Criegern (alle), Laris Bäucker (er/ihm): Partizipativer künstlerischer Beitrag “Mussage” <br> <br>
-            18:15 Uhr: Ausklang, <!-- Mehrlichtmusik-Festival, 19Uhr im Studiosaal der HfM Hanns Eisler Berlin (Charlottenstraße 55, 10117 Berlin) --> <br> <br> 
-            20:00 Uhr: Party in der Humboldt-Universität Berlin <br> <br>
+            17:15 Uhr: Workshop | Leander von Criegern (alle), Laris Bäucker (er/ihm): Partizipativer künstlerischer Beitrag “Mussage” <br> <br>
+            18:15 Uhr: Ausklang | Im Anschluss kann das MEHRLICHT!MUSIK 2026 – Ein Festival für neue Kompositionen von Studierenden besucht werden. <br> 19 Uhr im Studiosaal der HfM Hanns Eisler Berlin (Charlottenstraße 55, 10117 Berlin). <br> <br> 
+            20:00 Uhr: Fachschaftsparty im Institut für Musikwissenschaft der Humboldt Universität zu Berlin <br> <br>
         </p>
     </div>
     <div class="px-10 md:px-30 pt-20">
@@ -58,15 +58,16 @@
             Raum 501</h3>
         <p class="pt-10 leading-[1.8]">
             09:00 Uhr: Frühstück <br> <br>
-            10:00 Uhr: Vortrag, Julia Lenart (sie/ihr), Felix Dieterle (er/ihm): Warum Musikfamilienforschung historiographiekritisch ist<br> <br>
-            10:30 Uhr: Vortrag, Tilman Hinderling (er/ihm): »Männlicher Ernst, Formkraft, Neigung zur Romantik und feine weibliche Empfindung«. Die Komponistin Mathilde Kralik im Wiener Rundfunk 1927–1941 <br> <br>
-            11:00 Uhr: Vortrag, Paula Rösner (sie/ihr): Jazzfrauen in der DDR – Zwischen Identität und Ideologie <br> <br>
+            10:00 Uhr: Vortrag | Julia Lenart (sie/ihr), Felix Dieterle (er/ihm): Warum Musikfamilienforschung historiographiekritisch ist<br> <br>
+            10:30 Uhr: Vortrag | Tilman Hinderling (er/ihm): »Männlicher Ernst, Formkraft, Neigung zur Romantik und feine weibliche Empfindung«. Die Komponistin Mathilde Kralik im Wiener Rundfunk 1927–1941 <br> <br>
+            11:00 Uhr: Vortrag | Paula Rösner (sie/ihr): Jazzfrauen in der DDR – Zwischen Identität und Ideologie <br> <br>
             11:30 Uhr: Bewegte Pause <br> <br>
-            12:00 Uhr: Vortrag, Han Körner (dey/demm/deren), Beeke Hölzer (sie/ihr): Critical Classics: Auf der Suche nach konstruktiven Umgangsformen mit Diskriminierung in kanonischen Werken – oder: Was zur Hölle machen wir mit diesem Stück?! <br> <br>
-            12:45 Uhr: Workshop, Eli Vardzhiyska (sie/ihr): Gemeinsame Reflexion  <br> <br>
+            12:00 Uhr: Vortrag | Han Körner (dey/demm/deren), Beeke Hölzer (sie/ihr): Critical Classics: Auf der Suche nach konstruktiven Umgangsformen mit Diskriminierung in kanonischen Werken – oder: Was zur Hölle machen wir mit diesem Stück?! <br> <br>
+            12:45 Uhr: Workshop | Eli Vardzhiyska (sie/ihr): Gemeinsame Reflexion  <br> <br>
             13:30 Uhr: Ausklang <br> <br>
             14:00 Uhr: Mittagspause <br> <br>
-            15:00 Uhr: Bundesfachschaftstagung DVSM 
+            15:00 Uhr: Tagung der Bundesfachschaft Musikwissenschaften (BFM) <br> <br>
+            17:00 Uhr: Mitgliederversammlung des Dachverbandes der Studierenden der Musikwissenschaften e.V. (DVSM)
         </p>
     </div>
 </template>
