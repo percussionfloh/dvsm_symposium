1d28fe13437b2344fa5a8e5390227b37cab25877
percussionfloh <florian.moldaschl@web.de>
2026-09-25 12:01:33 +0200

    Logos gleiche Höhe

--- app/components/TheFooter.vue
+++ app/components/TheFooter.vue
@@ -11,7 +11,7 @@ const currentYear = new Date().getFullYear()
             <!-- <img src="/assets/fu_logo.png" class="max-h-12 md:max-h-35" alt="Logo FU"> -->
         </div>
         <div class="flex p-4">
-            <img src="/assets/udk_logo.png" class="max-h-10 md:max-h-30" alt="Logo UDK">
+            <img src="/assets/udk_logo.png" class="max-h-12 md:max-h-35" alt="Logo UDK">
         </div>
     </div>
     <div class="border-t border-gray-200 py-6">
